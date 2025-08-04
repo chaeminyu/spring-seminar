a655c4cbdc05d6a7de0417e2c5cc268b9c899e42
Chaemin Yu <chaemin.u@gmail.com>
2025-08-04 21:36:30 +0900

    feat: init

---
+++ src/main/java/org/likelion/springweek4/SpringWeek4Application.java
@@ -0,0 +1,13 @@
+package org.likelion.springweek4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeek4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/likelion/springweek4/SpringWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.springweek4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeek4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
